d6503e15c99299df2bd11b04ced21bde77295d1a
Shenghang Tsai <jackalcooper@gmail.com>
2026-08-21 20:14:22 +0800

    test(jason): gate captured codegen mapper (#359)

--- test/probe/jason/capability_matrix_test.exs
+++ test/probe/jason/capability_matrix_test.exs
@@ -35,7 +35,7 @@ defmodule Batata.Probe.Jason.CapabilityMatrixTest do
     assert capabilities["stdlib.lists_lookup_reverse"]["scope"] =~ ":lists.keyfind/3"
     assert capabilities["stdlib.enum_term_callbacks"]["status"] == "executable"
     assert capabilities["stdlib.enum_term_callbacks"]["gate"] == "codegen_pattern_mapper_test"
-    assert capabilities["stdlib.enum_term_callbacks"]["scope"] =~ "capture-free"
+    assert capabilities["stdlib.enum_term_callbacks"]["scope"] =~ "up to four tagged captures"
     assert capabilities["control.short_circuit_and"]["status"] == "executable"
     assert capabilities["control.short_circuit_and"]["gate"] == "execute_test"
 
